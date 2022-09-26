bd3153a560c962428b617a08baaab7d2fb3e7d76
GiadaFrr <114415608+GiadaFrr@users.noreply.github.com>
2022-09-26 20:57:16 +0200

    Versione 1.0 dell'esercizio array1.c

---
+++ array1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  int vet[] = {1,2,3};
+  int i;
+  
+  for(i=0;i<5;i++){
+    printf("vet[%d] = %d\n", i, vet[i];
+  }
+           printf("\n");
+           return 0;
+}
